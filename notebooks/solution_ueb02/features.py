11f3a2db183359d281142df873eaf1ec11a873f3
ufelm <ufelm@users.noreply.github.com>
2017-06-08 18:21:48 +0200

    Merged new features into features 

--- notebooks/solution_ueb02/features.py
+++ notebooks/solution_ueb02/features.py
@@ -460,3 +460,54 @@ def rf_weighted_average( p_list ):
     n_df.columns = [p_list[0].columns[0]]
     
     return n_df
+
+## new features
+## including: dimensionality reduction and new features
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+def reduce_dim_LDA(data, n_comps=25, label="target"):
+    """
+    Reduce the Dataframe to n_comps components 
+    keeping the label column for LDA
+    """
+    
+    if label is not None:
+        y = data[label]
+    
+    X = data.drop(label, axis=1)
+    
+    lda = LinearDiscriminantAnalysis(n_components=n_comps)
+    
+    X_reduced = pd.DataFrame(lda.fit(X, y).transform(X))
+    X_reduced.columns = ["lda_"+str(i) for i in range(len(X_reduced.columns))]
+    
+    if label is not None:
+        return  pd.concat( [X_reduced, y], axis=1 )
+    
+    return X_reduced
+   
+from sklearn.decomposition import PCA
+
+def reduce_dim_PCA(data, n_comps=10, label=None):
+    """
+    Reduce the Dataframe to n_comps components 
+    leaves label intact
+    """
+    
+    if label is not None:
+        y = data[label]
+        X = data.drop(label, axis=1)
+    else:
+        X = data
+    
+    pca = PCA(n_components=n_comps)
+    X_reduced = pd.DataFrame(pca.fit(X).transform(X))
+    X_reduced.columns = ["pca_"+str(i) for i in range(len(X_reduced.columns))]
+    
+    if label is not None:
+        return  pd.concat( [X_reduced, y], axis=1 )
+    
+    return X_reduced
+
+        
+    
